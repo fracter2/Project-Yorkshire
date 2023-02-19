c78961c245278793b11bb4182446fde472a2541e
fracter <fracter.giftogod@gmail.com>
2023-02-19 23:11:14 +0100

    Player Movement Made Smoother (Sorry!)
    I shoulda made a separate branch for dis.

--- src/Player_Character/Scripts/Player.gd
+++ src/Player_Character/Scripts/Player.gd
@@ -3,10 +3,17 @@ extends CharacterBody2D
 @export var speed: float = 6000
 @export var speed_max: float = 32000
 @export var drag:float = 0.8
+var input_dir := Vector2.ZERO
+
+@onready var Visuals := $Polygon2D
+@onready var prev_global_pos := Vector2.ZERO
+@onready var prev_global_pos2 := Vector2.ZERO
+var custom_velocity := Vector2.ZERO
+
 
 
 func _physics_process(delta):
-	var input_dir: Vector2 = Vector2.ZERO
+	input_dir = Vector2.ZERO
 	input_dir.y = Input.get_axis("move_up", "move_down")
 	input_dir.x = Input.get_axis("move_left", "move_right")
 	input_dir = input_dir.normalized()
@@ -16,14 +23,10 @@ func _physics_process(delta):
 	velocity.x = clamp(velocity.x, -speed_max, speed_max)
 	velocity.y = clamp(velocity.y, -speed_max, speed_max)
 	
+	prev_global_pos = global_position
 	
 	move_and_slide()
 
 
 func _process(delta):
-	# Implement a fix for movement jitter
-	pass
-	
-
-#func camera_update(var state:int = 0, var val:float = -1):
-#	pass
+	Visuals.global_position = prev_global_pos.lerp(global_position, Engine.get_physics_interpolation_fraction())
