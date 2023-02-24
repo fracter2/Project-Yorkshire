f7b8824a8565f3e4ce3af190ddd5b1e1a85d1a8a
Wolfxela <alexthegreitsas@gmail.com>
2023-02-24 17:41:07 +0200

    made the weapons able to reload

--- src/Ammo/Script/bullet.gd
+++ src/Ammo/Script/bullet.gd
@@ -1,12 +1,11 @@
 extends RigidBody2D
-
-var speed = 300
+var bullet = preload("res://src/Ammo/bullet.tscn")
+var speed = 100
 var velocity = Vector2(-1,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	apply_impulse(velocity.normalized() * speed)
-	print(transform)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
--- src/Weapons/Scripts/weapon_prototype.gd
+++ src/Weapons/Scripts/weapon_prototype.gd
@@ -1,7 +1,9 @@
 extends StaticBody2D
 
 var bullet = preload("res://src/Ammo/bullet.tscn")
-
+var reload = 0;
+var reloadTime = 0.1;
+var reloadReady = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,9 +11,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("action_primary"): shoot()
+	reload -= delta;
+	if (Input.is_action_pressed("action_primary") && reload < 0):
+		shoot()
+		reload = reloadTime
 	
-
 func shoot():
 	var bullet_Instance = bullet.instantiate()
 	
