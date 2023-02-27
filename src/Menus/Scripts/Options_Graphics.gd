fc1dfa54bf13c2d67479dcea7aa55781dd51eee4
fracter <fracter.giftogod@gmail.com>
2023-02-27 17:02:49 +0100

    Options Improvements + Pause menu -> Main menu btn

---
+++ src/Menus/Scripts/Options_Graphics.gd
@@ -0,0 +1,13 @@
+extends Panel
+
+@onready var Fullscreen_Modes = $Options_Graphics/List/Fullscreen_Modes
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Later, make sure the options are synced with any saved settings from files.
+	pass
+	
+
+func _on_fullscreen_modes_item_selected(index):
+	DisplayServer.window_set_mode(index)
+	# This relies ont he fact that the Indexes match with the DisplayServer Window Modes enum
--- src/Menus/Scripts/Pause_Menu.gd
+++ src/Menus/Scripts/Pause_Menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var options_scene = preload("res://src/Menus/Options.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,11 +15,16 @@ func _on_resume_button_down():
 
 
 func _on_options_button_down():
-	pass # Replace with function body.
+	var instance = options_scene.instantiate()
+	$CanvasLayer/MarginContainer.add_child(instance)
 
 
 func _on_main_menu_button_down():
-	pass # Replace with function body.
+	# if multiplayer, EXIT SERVER
+	get_tree().change_scene_to_file("res://src/Menus/Main Menu.tscn")
+	PauseMenu.in_pause_menu = false
+	queue_free()
+	
 
 
 func _on_quit_button_down():
