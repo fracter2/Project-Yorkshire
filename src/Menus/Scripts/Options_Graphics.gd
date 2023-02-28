c0f2719b0491e8c381dd47721baa726517ff870d
fracter <fracter.giftogod@gmail.com>
2023-02-28 01:52:13 +0100

    Finishing Fixes to options

--- src/Menus/Scripts/Options_Graphics.gd
+++ src/Menus/Scripts/Options_Graphics.gd
@@ -1,8 +1,6 @@
 extends Panel
 
-@onready var Fullscreen_Modes = $Options_Graphics/List/Fullscreen_Modes
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	# Later, make sure the options are synced with any saved settings from files.
 	pass
@@ -11,6 +9,3 @@ func _ready():
 func _on_fullscreen_modes_item_selected(index):
 	# This relies ont he fact that the Indexes match with the DisplayServer Window Modes enum
 	DisplayServer.window_set_mode(index)
-	DisplayServer.clipboard_get()
-	
-	#DisplayServer.border
