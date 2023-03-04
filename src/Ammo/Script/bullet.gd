c09f145358b323f83eeb39fd37808579e3d5b0ec
Wolfxela <alexthegreitsas@gmail.com>
2023-03-05 00:21:29 +0200

    finished the weapon system, next step optimising it for pickup

--- src/Ammo/Script/bullet.gd
+++ src/Ammo/Script/bullet.gd
@@ -1,12 +1,1 @@
-extends RigidBody2D
-var bullet = preload("res://src/Ammo/bullet.tscn")
-var speed = 100
-var velocity = Vector2(-1,0)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	apply_impulse(velocity.normalized() * speed)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	pass
--- src/Weapons/Scripts/weapon_prototype.gd
+++ src/Weapons/Scripts/weapon_prototype.gd
@@ -1,9 +1,11 @@
 extends StaticBody2D
 
 var bullet = preload("res://src/Ammo/bullet.tscn")
+
 var reload = 0;
 var reloadTime = 0.1;
-var reloadReady = true
+var bullet_Speed = 1000;
+var reloadReady = true;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,13 +14,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	reload -= delta;
+	rotation = get_global_mouse_position().angle_to_point(position)
 	if (Input.is_action_pressed("action_primary") && reload < 0):
 		shoot()
 		reload = reloadTime
 	
+	
+	
 func shoot():
 	var bullet_Instance = bullet.instantiate()
-	
-	get_parent().add_child(bullet_Instance)
+	bullet_Instance.global_position = get_node("Marker2D").global_position
+	bullet_Instance.rotation_degrees = rotation_degrees
+	bullet_Instance.apply_impulse(Vector2(-bullet_Speed,0).rotated(rotation))
+	get_tree().get_root().add_child(bullet_Instance)
 	
 
